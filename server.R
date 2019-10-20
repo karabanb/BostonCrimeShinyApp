51fba163643ea045f4c62a282eb8493655a81374
karabanb <bkarabands@gmail.com>
2019-10-20 16:45:15 +0200

    verison without working plots

--- server.R
+++ server.R
@@ -4,6 +4,8 @@
 library(shiny)
 library(tidyverse)
 library(tibbletime)
+library(zoo)
+
 
 #### LOADING DATA ######################################################################################################
 
@@ -15,15 +17,37 @@ shinyServer(function(input, output) {
   
   #### Prepraring data for reactive filtering -------------------------------------------------------------------------- 
   
+  # aggregared_data   <- reactive(
+  #   {prepared_data %>%
+  #       as_tbl_time(OCCURRED_ON_DATE) %>%
+  #       collapse_by(period = input[['period']]) %>%
+  #       group_by(as.character(OCCURRED_ON_DATE), CODE_GROUP) %>%
+  #       summarise(n = n()) %>%
+  #       rename(period = `as.character(OCCURRED_ON_DATE)`) %>%
+  #       mutate(period = ifelse(input[['period']] == 'yearly', year(period),
+  #                         ifelse(input[['period']] == 'quarterly', quarter(period, with_year = TRUE),
+  #                             format(period,'%Y-%m')
+  #                       )
+  #         )
+  #       )
+  #   }
+  # 
+  # )
+
+  
   aggregared_data   <- reactive(
     {prepared_data %>%
-        as_tbl_time(OCCURRED_ON_DATE) %>%
-        collapse_by(period = input[['period']]) %>%
-        group_by(as.character(OCCURRED_ON_DATE), CODE_GROUP) %>%
-        summarise(n = n()) %>%
-        rename(period = `as.character(OCCURRED_ON_DATE)`)}
+        mutate(period = as.character(ifelse(input[['period']] == 'yearly', year(OCCURRED_ON_DATE),
+                               ifelse(input[['period']] == 'quarterly', quarter(OCCURRED_ON_DATE, with_year = TRUE),
+                                      format(OCCURRED_ON_DATE,'%Y-%m')
+                                      )
+                               )
+               )) %>%
+        group_by(period, CODE_GROUP) %>%
+        summarise(n = n())
+      
+    }
   )
-
   
   #### Drawing stacked barplot -----------------------------------------------------------------------------------------
   
@@ -31,7 +55,9 @@ shinyServer(function(input, output) {
     data <- aggregared_data()
     ggplot(data, aes(period, n, fill = CODE_GROUP)) +
       geom_col(position = 'stack') +
-      theme_bw()
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 90))
+      
     })
    
   
@@ -41,7 +67,8 @@ shinyServer(function(input, output) {
      data <- aggregared_data()
      ggplot(data, aes(period, n, fill = CODE_GROUP)) +
        geom_col(position = 'fill') +
-       theme_bw()
+       theme_bw() +
+       theme(axis.text.x = element_text(angle = 90))
      })
 })
 
